7a8160011eb4554575e75662e139a7d1f58f2551
Sao I Kuan <saoikuan@gmail.com>
2022-09-21 19:50:39 -0400

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var ResultLable: UILabel!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func OperatorButton_Pressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func NumberButton_Pressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func ExtraButton_Pressed(_ sender: Any) {
+    }
+    
+}
+
