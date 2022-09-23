cf3657a02018303dcae723d876fcbe14426c2fae
Sao I Kuan <saoikuan@gmail.com>
2022-09-22 23:54:46 -0400

    Update ViewController.swift

--- ViewController.swift
+++ ViewController.swift
@@ -4,20 +4,22 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var ResultLable: UILabel!
     
-    override func viewDidLoad()
-    {
-        super.viewDidLoad()
-    }
-    
-    @IBAction func OperatorButton_Pressed(_ sender: Any) {
+
+    @IBAction func OperatorButton_Pressed(_ sender: UIButton) {
+        
     }
     
     
-    @IBAction func NumberButton_Pressed(_ sender: Any) {
+    @IBAction func NumberButton_Pressed(_ sender: UIButton) {
+        var button = sender as UIButton
+        ResultLable.text = button.titleLabel!.text
+        
+        button.titleLabel?.isHidden=true
     }
     
     
-    @IBAction func ExtraButton_Pressed(_ sender: Any) {
+    @IBAction func ExtraButton_Pressed(_ sender: UIButton) {
+        
     }
     
 }
